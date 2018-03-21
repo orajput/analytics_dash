406c6646763943195ea2172ca2f610104827ac0e
Omer Rajput <omer.rajput@gmail.com>
2018-03-21 18:08:14 +0100

    minor

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ Created on Wed Mar 21 16:53:28 2018
 @author: omer
 """
 
-# -*- coding: utf-8 -*-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -13,14 +12,15 @@ import dash_html_components as html
 import pandas as pd
 import numpy as np
 
-# Assign spreadsheet filename to `file`
+# Spreadsheet filename
 filename = '../data/Upload_Sheet_12_Mar_V2.xlsx'
 
-# Open a workbook 
+# Read spreadsheet
 row_id_headers = 1
 skiprows = 0
 data_sheet = pd.read_excel(filename, sheet_name='DATA', skiprows=skiprows, header=row_id_headers)
 
+# Created and resolved issues
 created_dates_slice = data_sheet['Created Date']
 resolved_dates_slice_filtered = data_sheet[u'Resolution Date®'][data_sheet['Status'] == 'Closed']
     
@@ -34,15 +34,15 @@ lastDate = max([created_dates_rounded[-1], resolved_dates_rounded[-1]])
 dates_bins = pd.date_range(firstDate, lastDate+pd.DateOffset(1))
 createdQ_daily_histogram = np.histogram(created_dates_rounded.values.astype('int64'), dates_bins.values.astype('int64'))
 resolvedQ_daily_histogram = np.histogram(resolved_dates_rounded.values.astype('int64'), dates_bins.values.astype('int64'))
+unresolved_queries_daily = np.cumsum(createdQ_daily_histogram[0] - resolvedQ_daily_histogram[0])
 
 # weekly
 weeks_bins = np.array(range(firstDate.week, lastDate.week+2))
 createdQ_weekly_histogram = np.histogram(created_dates_rounded.week.values, weeks_bins)
 resolvedQ_weekly_histogram = np.histogram(resolved_dates_rounded.week.values, weeks_bins)
-
-unresolved_queries_daily = np.cumsum(createdQ_daily_histogram[0] - resolvedQ_daily_histogram[0])
 unresolved_queries_weekly = np.cumsum(createdQ_weekly_histogram[0] - resolvedQ_weekly_histogram[0])
 
+# dashboard app
 app = dash.Dash()
 
 app.layout = html.Div(children=[
